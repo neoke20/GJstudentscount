3f691310927707d3abeb524730f2441517a82a52
Kevin Konczak <konczakkevin@gmail.com>
2022-06-14 16:24:55 +0900

    Added table with students data

--- parseStudents.js
+++ parseStudents.js
@@ -1,4 +1,18 @@
 fetch("./students.json")
     .then(Response => Response.json())
     .then(data => {
+      console.log(data);
+  		data.forEach((element, index) => {
+        document.getElementById("student-table").insertAdjacentHTML('beforeend', `<tr>
+        <th scope="row">${index + 1}</th>
+        <td>${element[`Given Names`]}</td>
+        <td>${element[`Family Name`]}</td>
+        <td>${element.Agent}</td>
+        <td>${element[`Course Start`]}</td>
+        <td>${element[`Course End`]}</td>
+        <td>${element[`Current Center`]}</td>
+        <td class="email">${element[`Contact Email`]}</td>
+        <td>${element[`Visa Type`]}</td>
+      </tr>`)
+      });
     });

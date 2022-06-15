78a0d0974e70ef2a78195c5a47e8dd21dc1fcb53
Kevin Konczak <konczakkevin@gmail.com>
2022-06-15 15:49:13 +0900

    Add to counter when start date = date

--- assets/js/parseStudents.js
+++ assets/js/parseStudents.js
@@ -1,5 +1,7 @@
-const dates =
-  [
+fetch("./students.json")
+    .then(Response => Response.json())
+    .then(data => {
+      let dates = [
     {"date":"20/06/2022",
      "counter":0},
     {"date":"27/06/2022",
@@ -21,20 +23,16 @@ const dates =
      {"date":"22/08/2022",
      "counter":0},
   ];
-
-// Display all the dates we are tracking
-dates.forEach(item => {
-  document.getElementById('date-table').insertAdjacentHTML('beforeend', `<p>${item.date}: ${item.counter}</p>`);
-});
-
-fetch("./students.json")
-    .then(Response => Response.json())
-    .then(data => {
       // Get the length of array to know total students
       const totalStudents = data.length;
       document.getElementById("total-students").insertAdjacentText('afterbegin', totalStudents);
       // Collecting data iterating over Array to create table and count students
       data.forEach((element, index) => {
+
+        dates.forEach(item => {
+          item.date == element[`Course Start`] ? item.counter++ : "";
+        });
+
         document.getElementById("student-table").insertAdjacentHTML('beforeend', `<tr>
         <th scope="row">${index + 1}</th>
         <td>${element[`Given Names`]}</td>
@@ -46,4 +44,8 @@ fetch("./students.json")
         <td class="email">${element[`Contact Email`]}</td>
       </tr>`)
       });
+          // Display all the dates we are tracking
+dates.forEach(item => {
+  document.getElementById('date-table').insertAdjacentHTML('beforeend', `<p>${item.date}: ${item.counter}</p>`);
+});
     });

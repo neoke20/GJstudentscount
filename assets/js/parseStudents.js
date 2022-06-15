0d9a535190bbd5c87416afca9ee0962b4192e5b8
Kevin Konczak <konczakkevin@gmail.com>
2022-06-15 13:34:16 +0900

    removing the July students as this was just a test

--- assets/js/parseStudents.js
+++ assets/js/parseStudents.js
@@ -32,11 +32,9 @@ fetch("./students.json")
     .then(data => {
       // Get the length of array to know total students
       const totalStudents = data.length;
-      let julyIntake = 0;
       document.getElementById("total-students").insertAdjacentText('afterbegin', totalStudents);
       // Collecting data iterating over Array to create table and count students
       data.forEach((element, index) => {
-        element[`Course Start`] == "01/08/2022" ? julyIntake++ : julyIntake;
         document.getElementById("student-table").insertAdjacentHTML('beforeend', `<tr>
         <th scope="row">${index + 1}</th>
         <td>${element[`Given Names`]}</td>
@@ -48,5 +46,4 @@ fetch("./students.json")
         <td class="email">${element[`Contact Email`]}</td>
       </tr>`)
       });
-      document.getElementById("july-students").insertAdjacentText('afterbegin', julyIntake);
     });

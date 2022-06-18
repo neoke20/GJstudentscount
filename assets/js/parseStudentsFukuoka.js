69cfea6295ff112c88fc564b6beeb0aa26ea8a08
Kevin Konczak <konczakkevin@gmail.com>
2022-06-18 09:58:02 +0900

    removed the information in the parsing js

--- assets/js/parseStudentsFukuoka.js
+++ assets/js/parseStudentsFukuoka.js
@@ -291,13 +291,9 @@ fetch("./studentsFukuoka.json")
           document.getElementById("student-table").insertAdjacentHTML('beforeend', `<tr>
           <th scope="row">${element[`Student ID`]}</th>
           <td>${element[`Given Names`]}</td>
-          <td>${element[`Family Name`]}</td>
-          <td class="jp-name">${element[`Japanese Name`]}</td>
-          <td>${element.Agent}</td>
           <td>${element[`Course Start`]}</td>
           <td>${element[`Course End`]}</td>
           <td>${element[`Current Center`]}</td>
-          <td class="email">${element[`Contact Email`]}</td>
         </tr>`)
         }
       });
